9f544e468493aad824cfe79a7a13e28f4b39ba34
Atharva Shinde <144154083+Atharva680@users.noreply.github.com>
2026-09-13 20:38:22 +0530

    Time: 387 ms (40.68%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,7 +1,3 @@
---select  name , population ,area
---from world
---where population between 25000000 and 38000000
-
 SELECT name,population,area
 FROM world
 WHERE area >= 3000000 OR population >= 25000000;
